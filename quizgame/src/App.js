92bc10dc6466fcb6308cc9a2485b88e284503789
Zacharey Carey <zac4ncstate@aol.com>
2020-07-27 14:24:15 -0400

    adding functionality to the questions

--- quizgame/src/App.js
+++ quizgame/src/App.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IndexcardList from "./IndexcardList";
 import "./indexcard.css";
+import axios from "axios";
 
 function App() {
   const [indexcards, setIndexcards] = useState(WARMUP_INDEXCARDS);
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setIndexcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer;
+          const options = [...questionItem.incorrect_answers, answer];
+          return {
+            id: "${index}-$(Date.now()}",
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
   return <IndexcardList indexcards={indexcards} />;
 }
 
